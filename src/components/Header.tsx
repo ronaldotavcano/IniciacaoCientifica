ba240740a3e635b2b60bd8aeda8dec986a03bf25
Ronaldo T Cano <ronaldotavarescano@gmail.com>
2026-05-07 16:44:38 -0300

    Subindo tudo, mudando p/ linux

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+export default function Header(){
+    return(
+        <div>
+            <nav>
+                <a href="">Início</a>
+                <a href="">Tutorial</a>
+                <a href="">Sobre</a>
+            </nav>
+        </div>
+    )
+}
